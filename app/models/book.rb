7abc2d599ee4e6f27d5859549ae88b5eeb5dec1b
Alexandru <rammazzoti2000@gmail.com>
2020-09-08 15:52:24 +0200

    book-model: Add validations for book model properties

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :pages, presence: true
+  validates :category, presence: true
+  validates :progress, presence: true
+  default_scope { order(created_at: :desc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_08_132239) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
