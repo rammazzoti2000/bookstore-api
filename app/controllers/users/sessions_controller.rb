144c23f69b55e332b0416e6bd47bb4dc7760f3a9
Alexandru <rammazzoti2000@gmail.com>
2020-09-12 14:32:15 +0200

    api: Rack Cors fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,33 +1,47 @@
-class Api::V1::Users::SessionsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(email: params[:user][:email])
-    if @user&.authenticate(params[:user][:password])
-      login @user
+    @user = User.find_by(email: session_params[:email])
+
+    if @user && @user.authenticate(session_params[:password])
+      login!
       render json: {
-        code: 200,
-        user: {
-          id: @user.id,
-          name: @user.name
-        }
+        logged_in: true,
+        user: @user
       }
     else
       render json: {
-        code: 400,
-        errors: { Error: ['Email and/or Password does not match !'] }
+        status: 401,
+        errors: ['no such user', 'verify credentials and try again or signup']
+      }
+    end
+  end
+
+  def is_logged_in?
+    if logged_in? && current_user
+      render json: {
+        logged_in: true,
+        user: current_user
+      }
+    else
+      render json: {
+        logged_in: false,
+        message: 'no such user'
       }
     end
   end
 
   def destroy
-    @current_user = nil
-    session.delete(:id)
+    logout!
+    render json: {
+      status: 200,
+      logged_out: true
+    }
   end
 
   private
 
-  def user_params
-    params[:user].permit(:email, :password)
+  def session_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
